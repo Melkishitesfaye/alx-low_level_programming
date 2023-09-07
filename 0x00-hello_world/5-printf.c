68e62cb9adee78b313b159c2d46aa42e910dcbf4
melki <melkishitesfayeangassa@gmial.com>
2023-09-07 09:09:24 -0700

    5. Heloo, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -A program using printf f to display a string
+ * Return: 0 (success)
+ */
+
+void main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
