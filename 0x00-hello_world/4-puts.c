26cece24a944de4f02cd2ba3a317c8cc7f9cb29b
melki <melkishitesfayeangassa@gmial.com>
2023-09-07 09:00:43 -0700

    4. hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
-#!/bin/bash
-
-int main(){
+#include <stdio/h>
+/**
+ * main - A program that uses  puts function to print a string
+ * Return: 0 (success)
+ */
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
