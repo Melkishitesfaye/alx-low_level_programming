d1496119f9cb8cfd00dd56bcae60811cec325538
melki <melkishitesfayeangassa@gmial.com>
2023-09-16 02:00:58 -0700

    6-lines.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal.
+ * @n: int to be checked
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int i = 0;
+
+	for (; i < n; i++)
+	       _putchar('_');
+	_putchar('\n');
+}
