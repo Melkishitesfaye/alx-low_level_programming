bc34cf80b04e49fa98183ed86a68394ff748939c
melki <melkishitesfayeangassa@gmial.com>
2023-09-16 05:24:18 -0700

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,15 +7,15 @@
 
 int main(void)
 {
-	int i = 0;
+	int i = 1;
 
-		for (; i < 101; i++)
+		for (; i <= 100; i++)
 		{
-			if (i % 3 == 0 && i % 5 == 0)
+			if ((i % 3) == 0 && (i % 5) == 0)
 				printf("FizzBuzz ");
-			else if (i % 3 == 0)
+			else if ((i % 3) == 0)
 				printf("Fizz ");
-			else if (i % 5 == 0)
+			else if ((i % 5) == 0)
 				printf("Buzz ");
 			else
 				printf("%d ", i);
