c1db718de3ab36e447bdcc9152910e79a53f467a
melki <melkishitesfayeangassa@gmail.com>
2023-09-30 13:42:04 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - prints a string
+ * @s: string to be printed
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
